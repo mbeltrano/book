8b8b22abe8bc5fcf3ef48bc33c6e8548118efb45
Mattia Beltrano <mattia.beltrano@external.a2a.it>
2026-05-12 19:20:38 +0200

    added table and showBooks method

--- script.js
+++ script.js
@@ -23,7 +23,30 @@ function addBookToLibrary(title,author,pages, read) {
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", false);
+
+addBookToLibrary("Certamente", "R. Bellantonio", "1520", true);
+
+addBookToLibrary("Lello", "Solo Lello", "15", true);
+
 console.log(myLibrary);
+
+function showBooks(books){
+    const table = document.getElementById("table-body");
+    
+    for(const book of books){
+      const newRow = document.createElement("tr");
+      console.log(book);
+        newRow.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+    `;
+    
+    table.appendChild(newRow);
+    }
+}
+showBooks(myLibrary);
 
 //const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
 //console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
